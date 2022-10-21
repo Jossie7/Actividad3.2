9cd50a9e3479ca0b5b01988636833d52c45d9861
Jossie7 <jyovcon215@g.educaand.es>
2022-10-21 14:57:12 +0200

    Create Tareadenumeros.java

---
+++ Tareadenumeros.java
@@ -0,0 +1,18 @@
+public class Tareadenumeros{
+    public static void main(String[] args) {
+        //Variables
+        int numero;
+        numero = 8;
+        numero = 10;
+        numero = 7;
+
+        // Esto imprime la operación
+        if (numero%2==0){
+            System.out.println("Es par");
+        } 
+        else{
+            System.out.println("Es impar");
+        }
+        }
+}
+//Nos dará qué valor toma el último de los números, es decir si el último número es 10, va a imprimir que es par 
